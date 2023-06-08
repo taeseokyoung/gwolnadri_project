3e5121bc96e89887f69384834c4b1dfd2288f0c1
Jeongchan-ho <zmxndk1@gmail.com>
2023-06-08 10:40:22 +0900

    [추가] 행사정보 조회, 생성, 수정, 삭제 #16 #30 #31 #32
    config/urls.py : events 경로추가
    events/models.py : 행사정보 모델 추가
    events/serializers.py : 행사 조회, 생성, 수정, 삭제용 serializer 추가
    events/urls.py : event_view, event_detail_view url경로 추가
    events/views.py : EventView, EventDetailView 추가
    events/permissions.py : CustomPermission 추가

--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from users.serializers import UserTokenObtainPairSerializer, UserSerializer
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
 
-# Create your views here.
+
+# 회원가입
+class SignUpView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "가입완료!"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(
+                {"message": f"${serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+# 로그인
+class LoginView(TokenObtainPairView):
+    serializer_class = UserTokenObtainPairSerializer
