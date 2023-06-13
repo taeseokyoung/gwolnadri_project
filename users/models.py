ab87b90098619394dd8912c00ebda824f2423d36
BenCyera <dltpgml1124@gmail.com>
2023-06-13 21:44:45 +0900

    [수정] 한복점 상세보기 수정 #9
    config/settings.py : Rest_framework 중복 설정 삭제
    stores/urls.py : StoreDetailView url 통합
    users/models.py : Verify 모델 삭제

--- users/models.py
+++ users/models.py
@@ -56,8 +56,3 @@ class User(AbstractBaseUser):
         "Is the user a member of staff?"
         # Simplest possible answer: All admins are staff
         return self.is_admin
-
-
-class Verify(models.Model):
-    email = models.EmailField()
-    athnt_code = models.CharField(max_length=6)
