c538d83414cb50477e56825d881c89395655e9f8
mkim <minkyungkim09@gmail.com>
2023-06-30 23:25:31 +0900

    [추가] 로그아웃 추가 #1

--- users/urls.py
+++ users/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 from users import views
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
+    TokenObtainPairView,
 )
 
 urlpatterns = [
     path("signup/", views.SignupView.as_view(), name="signup"),
     path("token/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("me/", views.Me.as_view(), name="profile_view"),
     path("me/modify/", views.UpdateProfileView.as_view(), name="profile_modify"),
@@ -17,9 +19,9 @@ urlpatterns = [
         name="change_password",
     ),
     path("kakao/login/callback/", views.KakaoCallbackView.as_view(), name="kakao_callback"),
-    path("naver/login/callback/", views.naver_callback, name="naver_callback"),
-    path("naver/login/finish/", views.NaverLogin.as_view(), name="naver_login_drf"),
-    path("google/login/callback/", views.google_callback, name="google_callback"),
-    path("google/login/finish/", views.GoogleLogin.as_view(), name="google_login_drf"),
+    # path("naver/login/callback/", views.naver_callback, name="naver_callback"),
+    # path("naver/login/finish/", views.NaverLogin.as_view(), name="naver_login_drf"),
+    # path("google/login/callback/", views.google_callback, name="google_callback"),
+    # path("google/login/finish/", views.GoogleLogin.as_view(), name="google_login_drf"),
 ]
 
--- users/views.py
+++ users/views.py
@@ -80,6 +80,12 @@ def generate_jwt_token(user):
     refresh = CustomRefreshToken.for_user(user)
     return {"refresh": str(refresh), "access": str(refresh.access_token)}
 
+class LogoutView(APIView):
+    def post(self, request):
+        response = Response({"message": "로그아웃 완료"}, status=status.HTTP_204_NO_CONTENT)
+        response.delete_cookie("access")
+        response.delete_cookie("refresh")
+        return response
 
 # 마이페이지 보기
 class Me(APIView):
@@ -116,13 +122,12 @@ class UpdateProfileView(generics.UpdateAPIView):
         self.perform_update(serializer)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-
+    # 회원탈퇴
     def delete(self, request):
         user = request.user
         user.delete()
         return Response({"message": "회원 탈퇴 완료"}, status=status.HTTP_204_NO_CONTENT)
-
-
+    
 # 비밀번호 변경
 class ChangePasswordView(generics.UpdateAPIView):
     queryset = User.objects.all()
@@ -133,99 +138,6 @@ class ChangePasswordView(generics.UpdateAPIView):
 # 소셜로그인
 BASE_URL = os.environ.get("BASE_URL")
 FRONT_URL = os.environ.get("FRONT_URL")
-
-# GOOGLE Login
-
-GOOGLE_CALLBACK_URI = BASE_URL + "users/google/login/callback/"
-GOOGLE_REDIRECT_URI = FRONT_URL + "google.html"
-
-@api_view(["POST", "GET"])
-def google_callback(request):
-    client_id = os.environ.get("SOCIAL_AUTH_GOOGLE_CLIENT_ID")
-    client_secret = os.environ.get("SOCIAL_AUTH_GOOGLE_SECRET")
-    state = os.environ.get("STATE")
-    code = request.GET.get("code")
-
-    token_request = requests.post(
-        f"https://oauth2.googleapis.com/token?client_id={client_id}&client_secret={client_secret}&code={code}&grant_type=authorization_code&redirect_uri={GOOGLE_REDIRECT_URI}&state={state}"
-    )
-
-    token_request_json = token_request.json()
-    error = token_request_json.get("error")
-
-    if error is not None:
-        raise JSONDecodeError(error)
-
-    access_token = token_request_json.get("access_token")
-
-    email_request = requests.get(
-        f"https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={access_token}"
-    )
-    email_request_status = email_request.status_code
-
-    if email_request_status != 200:
-        return JsonResponse(
-            {"err_msg": "구글 이메일을 가져오지 못했습니다."}, status=status.HTTP_400_BAD_REQUEST
-        )
-
-    email_request_json = email_request.json()
-    email = email_request_json.get("email")
-
-    try:
-        user = User.objects.get(email=email)
-        social_user = SocialAccount.objects.get(user=user)
-
-        if social_user.provider != "google":
-            return JsonResponse(
-                {"err_msg": "일치하는 구글 계정이 없습니다. 확인해 주세요."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
-        data = {"access_token": access_token, "code": code}
-        accept = requests.post(f"{BASE_URL}users/google/login/finish/", data=data)
-        accept_status = accept.status_code
-
-        if accept_status != 200:
-            return JsonResponse({"err_msg": "구글 로그인에 실패했습니다."}, status=accept_status)
-
-        user, created = User.objects.get_or_create(email=email)
-        refresh_token = UserTokenObtainPairSerializer.get_token(user)
-        access_token = refresh_token.access_token
-        
-        return Response(
-            {"refresh": str(refresh_token), "access": str(access_token)},
-            status=status.HTTP_200_OK,
-        )
-
-    except User.DoesNotExist:
-        data = {"access_token": access_token, "code": code}
-        print("12번", data)
-        accept = requests.post(f"{BASE_URL}users/google/login/finish/", data=data)
-        accept_status = accept.status_code
-        print("13", accept_status)
-
-        if accept_status != 200:
-            return JsonResponse({"err_msg": "구글로 회원가입에 실패했습니다. 다시 시도해 주세요."}, status=accept_status)
-
-        user = User.objects.get_or_create(email=email)
-        refresh_token = UserTokenObtainPairSerializer.get_token(user)
-        access_token = refresh_token.access_token
-
-        return Response(
-            {"refresh": str(refresh_token), "access": str(access_token)},
-            status=status.HTTP_201_CREATED,
-        )
-    
-    except SocialAccount.DoesNotExist:
-        return JsonResponse(
-            {"err_msg": "이미 가입되어 있는 구글 계정입니다._G"},
-            status=status.HTTP_400_BAD_REQUEST,
-        )
-
-class GoogleLogin(SocialLoginView):
-    adapter_class = google_view.GoogleOAuth2Adapter
-    callback_url = GOOGLE_CALLBACK_URI
-    client_class = OAuth2Client
 
 # KAKAO Login
 KAKAO_CALLBACK_URI = BASE_URL + "users/kakao/login/callback/"
@@ -297,90 +209,182 @@ class KakaoCallbackView(APIView):
                 status=status.HTTP_200_OK,
             )
 
-# NAVER Login
-NAVER_CALLBACK_URI = BASE_URL + "/users/naver/login/callback/"
-NAVER_REDIRECT_URI = FRONT_URL + "naver.html"
+# # NAVER Login
+# NAVER_CALLBACK_URI = BASE_URL + "/users/naver/login/callback/"
+# NAVER_REDIRECT_URI = FRONT_URL + "naver.html"
 
-@api_view(["POST", "GET"])
-def naver_callback(request):
-    client_id = os.environ.get("SOCIAL_AUTH_NAVER_CLIENT_ID")
-    client_secret = os.environ.get("SOCIAL_AUTH_NAVER_SECRET")
-    state = os.environ.get("STATE")
-    code = request.GET.get("code")
+# @api_view(["POST", "GET"])
+# def naver_callback(request):
+#     client_id = os.environ.get("SOCIAL_AUTH_NAVER_CLIENT_ID")
+#     client_secret = os.environ.get("SOCIAL_AUTH_NAVER_SECRET")
+#     state = os.environ.get("STATE")
+#     code = request.GET.get("code")
 
-    token_request = requests.get(
-        f"https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id={client_id}&client_secret={client_secret}&code={code}&state={state}&redirect_uri={NAVER_REDIRECT_URI}"
-    )
+#     token_request = requests.get(
+#         f"https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id={client_id}&client_secret={client_secret}&code={code}&state={state}&redirect_uri={NAVER_REDIRECT_URI}"
+#     )
 
-    token_response_json = token_request.json()
-    error = token_response_json.get("error", None)
-    if error is not None:
-        raise JSONDecodeError(error)
+#     token_response_json = token_request.json()
+#     error = token_response_json.get("error", None)
+#     if error is not None:
+#         raise JSONDecodeError(error)
     
-    access_token = token_response_json.get("access_token")
+#     access_token = token_response_json.get("access_token")
 
-    profile_request = requests.post(
-        "https://openapi.naver.com/v1/nid/me",
-        headers={"Authorization": f"Bearer {access_token}"},
-    )
-    profile_request_json = profile_request.json()
-    print(profile_request_json)
-    email = profile_request_json.get("response").get("email")
+#     profile_request = requests.post(
+#         "https://openapi.naver.com/v1/nid/me",
+#         headers={"Authorization": f"Bearer {access_token}"},
+#     )
+#     profile_request_json = profile_request.json()
+#     print(profile_request_json)
+#     email = profile_request_json.get("response").get("email")
     
-    if email is None:
-        return JsonResponse(
-        {"err_msg": "네이버 이메일을 가져오지 못했습니다."}, status=status.HTTP_400_BAD_REQUEST
-        )
+#     if email is None:
+#         return JsonResponse(
+#         {"err_msg": "네이버 이메일을 가져오지 못했습니다."}, status=status.HTTP_400_BAD_REQUEST
+#         )
 
-    try:
-        user = User.objects.get(email=email)
-        social_user = SocialAccount.objects.get(user=user)
+#     try:
+#         user = User.objects.get(email=email)
+#         social_user = SocialAccount.objects.get(user=user)
 
-        if social_user.provider != "naver":
-            return JsonResponse(
-                {"err_msg": "일치하는 네이버 계정이 없습니다"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+#         if social_user.provider != "naver":
+#             return JsonResponse(
+#                 {"err_msg": "일치하는 네이버 계정이 없습니다"},
+#                 status=status.HTTP_400_BAD_REQUEST,
+#             )
 
-        data = {"access_token": access_token, "code": code}
-        accept = requests.post(f"{BASE_URL}users/naver/login/finish/", data=data)
-        accept_status = accept.status_code
+#         data = {"access_token": access_token, "code": code}
+#         accept = requests.post(f"{BASE_URL}users/naver/login/finish/", data=data)
+#         accept_status = accept.status_code
 
-        if accept_status != 200:
-            return JsonResponse({"err_msg": "네이버 로그인이 실패했습니다."}, status=accept_status)
+#         if accept_status != 200:
+#             return JsonResponse({"err_msg": "네이버 로그인이 실패했습니다."}, status=accept_status)
 
-        user, created = User.objects.get_or_create(email=email)
-        refresh_token = UserTokenObtainPairSerializer.get_token(user)
-        access_token = refresh_token.access_token
+#         user, created = User.objects.get_or_create(email=email)
+#         refresh_token = UserTokenObtainPairSerializer.get_token(user)
+#         access_token = refresh_token.access_token
 
-        return Response(
-            {"refresh": str(refresh_token), "access": str(access_token)},
-            status=status.HTTP_200_OK,
-        )
+#         return Response(
+#             {"refresh": str(refresh_token), "access": str(access_token)},
+#             status=status.HTTP_200_OK,
+#         )
 
-    except User.DoesNotExist:
-        data = {"access_token": access_token, "code": code}
-        accept = requests.post(f"{BASE_URL}users/naver/login/finish/", data=data)
-        accept_status = accept.status_code
+#     except User.DoesNotExist:
+#         data = {"access_token": access_token, "code": code}
+#         accept = requests.post(f"{BASE_URL}users/naver/login/finish/", data=data)
+#         accept_status = accept.status_code
 
-        if accept_status != 200:
-            return JsonResponse({"err_msg": "네이버로 회원가입이 실패했습니다."}, status=accept_status)
+#         if accept_status != 200:
+#             return JsonResponse({"err_msg": "네이버로 회원가입이 실패했습니다."}, status=accept_status)
         
-        user, created = User.objects.get_or_create(email=email)
-        refresh_token = UserTokenObtainPairSerializer.get_token(user)
-        access_token = refresh_token.access_token
-        return Response(
-            {"refresh": str(refresh_token), "access": str(access_token)},
-            status=status.HTTP_201_CREATED,
-        )
+#         user, created = User.objects.get_or_create(email=email)
+#         refresh_token = UserTokenObtainPairSerializer.get_token(user)
+#         access_token = refresh_token.access_token
+#         return Response(
+#             {"refresh": str(refresh_token), "access": str(access_token)},
+#             status=status.HTTP_201_CREATED,
+#         )
 
-    except SocialAccount.DoesNotExist:
-        return JsonResponse(
-            {"err_msg": "이미 가입되어 있는 네이버 계정입니다. 다시 시도해 주세요._N"},
-            status=status.HTTP_400_BAD_REQUEST,
-        )
+#     except SocialAccount.DoesNotExist:
+#         return JsonResponse(
+#             {"err_msg": "이미 가입되어 있는 네이버 계정입니다. 다시 시도해 주세요._N"},
+#             status=status.HTTP_400_BAD_REQUEST,
+#         )
 
-class NaverLogin(SocialLoginView):
-    adapter_class = naver_view.NaverOAuth2Adapter
-    callback_url = NAVER_CALLBACK_URI
-    client_class = OAuth2Client
+# class NaverLogin(SocialLoginView):
+#     adapter_class = naver_view.NaverOAuth2Adapter
+#     callback_url = NAVER_CALLBACK_URI
+#     client_class = OAuth2Client
+
+# # GOOGLE Login
+# GOOGLE_CALLBACK_URI = BASE_URL + "users/google/login/callback/"
+# GOOGLE_REDIRECT_URI = FRONT_URL + "google.html"
+
+# @api_view(["POST", "GET"])
+# def google_callback(request):
+#     client_id = os.environ.get("SOCIAL_AUTH_GOOGLE_CLIENT_ID")
+#     client_secret = os.environ.get("SOCIAL_AUTH_GOOGLE_SECRET")
+#     state = os.environ.get("STATE")
+#     code = request.GET.get("code")
+
+#     token_request = requests.post(
+#         f"https://oauth2.googleapis.com/token?client_id={client_id}&client_secret={client_secret}&code={code}&grant_type=authorization_code&redirect_uri={GOOGLE_REDIRECT_URI}&state={state}"
+#     )
+
+#     token_request_json = token_request.json()
+#     error = token_request_json.get("error")
+
+#     if error is not None:
+#         raise JSONDecodeError(error)
+
+#     access_token = token_request_json.get("access_token")
+
+#     email_request = requests.get(
+#         f"https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={access_token}"
+#     )
+#     email_request_status = email_request.status_code
+
+#     if email_request_status != 200:
+#         return JsonResponse(
+#             {"err_msg": "구글 이메일을 가져오지 못했습니다."}, status=status.HTTP_400_BAD_REQUEST
+#         )
+
+#     email_request_json = email_request.json()
+#     email = email_request_json.get("email")
+
+#     try:
+#         user = User.objects.get(email=email)
+#         social_user = SocialAccount.objects.get(user=user)
+
+#         if social_user.provider != "google":
+#             return JsonResponse(
+#                 {"err_msg": "일치하는 구글 계정이 없습니다. 확인해 주세요."},
+#                 status=status.HTTP_400_BAD_REQUEST,
+#             )
+
+#         data = {"access_token": access_token, "code": code}
+#         accept = requests.post(f"{BASE_URL}users/google/login/finish/", data=data)
+#         accept_status = accept.status_code
+
+#         if accept_status != 200:
+#             return JsonResponse({"err_msg": "구글 로그인에 실패했습니다."}, status=accept_status)
+
+#         user, created = User.objects.get_or_create(email=email)
+#         refresh_token = UserTokenObtainPairSerializer.get_token(user)
+#         access_token = refresh_token.access_token
+        
+#         return Response(
+#             {"refresh": str(refresh_token), "access": str(access_token)},
+#             status=status.HTTP_200_OK,
+#         )
+
+#     except User.DoesNotExist:
+#         data = {"access_token": access_token, "code": code}
+#         print("12번", data)
+#         accept = requests.post(f"{BASE_URL}users/google/login/finish/", data=data)
+#         accept_status = accept.status_code
+#         print("13", accept_status)
+
+#         if accept_status != 200:
+#             return JsonResponse({"err_msg": "구글로 회원가입에 실패했습니다. 다시 시도해 주세요."}, status=accept_status)
+
+#         user = User.objects.get_or_create(email=email)
+#         refresh_token = UserTokenObtainPairSerializer.get_token(user)
+#         access_token = refresh_token.access_token
+
+#         return Response(
+#             {"refresh": str(refresh_token), "access": str(access_token)},
+#             status=status.HTTP_201_CREATED,
+#         )
+    
+#     except SocialAccount.DoesNotExist:
+#         return JsonResponse(
+#             {"err_msg": "이미 가입되어 있는 구글 계정입니다._G"},
+#             status=status.HTTP_400_BAD_REQUEST,
+#         )
+
+# class GoogleLogin(SocialLoginView):
+#     adapter_class = google_view.GoogleOAuth2Adapter
+#     callback_url = GOOGLE_CALLBACK_URI
+#     client_class = OAuth2Client
