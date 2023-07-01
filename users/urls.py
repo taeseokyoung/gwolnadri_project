e694e8d86223952a8cca1dc67dfedb8c1c2b9522
Kyoung <105770887+taeseokyoung@users.noreply.github.com>
2023-07-02 00:01:33 +0900

    [삭제] 회원탈퇴 기능 주석처리 #6

--- users/urls.py
+++ users/urls.py
@@ -12,16 +12,15 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("me/", views.Me.as_view(), name="profile_view"),
     path("me/modify/", views.UpdateProfileView.as_view(), name="profile_modify"),
-    path("me/delete/", views.UpdateProfileView.as_view(), name="profile_delete"),
+    # path("me/delete/", views.UpdateProfileView.as_view(), name="profile_delete"),
     path(
         "me/<int:pk>/password/",
         views.ChangePasswordView.as_view(),
         name="change_password",
     ),
-    path("kakao/login/callback/", views.KakaoCallbackView.as_view(), name="kakao_callback"),
-    # path("naver/login/callback/", views.naver_callback, name="naver_callback"),
-    # path("naver/login/finish/", views.NaverLogin.as_view(), name="naver_login_drf"),
-    # path("google/login/callback/", views.google_callback, name="google_callback"),
-    # path("google/login/finish/", views.GoogleLogin.as_view(), name="google_login_drf"),
+    path(
+        "kakao/login/callback/",
+        views.KakaoCallbackView.as_view(),
+        name="kakao_callback",
+    ),
 ]
-
